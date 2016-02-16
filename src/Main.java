7fa134815a34043139eb26400038952a8a4525e4
MrFika <dat13vni@student.lu.se>
2016-02-16 14:53:57 +0100

    TimeTo test merge

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.math.BigInteger;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BigInteger i = new BigInteger("1232123123");
+		System.out.println(i);
+		i = i.add(new BigInteger("123123123"));
+		System.out.println(i);
+		i = i.mod(new BigInteger("23"));
+		System.out.println(i);
+		
+		if(i.testBit(0)){
+			System.out.println(i+"Sant");
+		}
+		i = i.add(BigInteger.ONE);
+		if(i.testBit(0)){
+			System.out.println(i+"Sant");
+		}else{
+			System.out.println("adasdasdsda");
+		}
+	}
+	
+	public boolean rabinMillerTest(BigInteger n){
+		// return true if n is probably prime, false if composite
+		
+		// Alla positiva heltal i intervallet 0<x<4 är primtal. Returnera true direkt.
+		if(n.compareTo(new BigInteger("3")) <= 0){
+			return true; 
+		}
+		
+		return false;
 	}
 
 }
