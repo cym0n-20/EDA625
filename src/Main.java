e486b1bd441d304224dabedbde887aa6fc12aaf0
cym0n-20 <simon.gustafsson20@hotmail.com>
2016-02-18 15:10:02 +0100

    assignment 2 klar!

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,40 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] primes = { "1", "3", "3613", "7297", "226673591177742970257407", "2932031007403" };
-		String[] nonPrimes = { "3341", "2932021007403", "226673591177742970257405" };
-		for (String p : primes) {
-			if (rabinMillerTest(new BigInteger(p), 512)) {
-				System.out.println("yes");
-			}
-		}
-		for (String p : nonPrimes) {
-			if (!rabinMillerTest(new BigInteger(p), 512)) {
-				System.out.println("yes no prime");
-			}
-		}
 		
+		ArrayList<BigInteger> list = generator(512);
 		System.out.println("complete");
 
 	}
 
-	public static boolean rabinMillerTest(BigInteger n, int bitlengh) {
+	public static boolean rabinMillerTest(BigInteger n, int bitlength) {
 		if (n.compareTo(new BigInteger("3")) < 0) {
 			return true;
 		}
 		BigInteger r = BigInteger.ZERO;
 		BigInteger s = n.subtract(BigInteger.ONE);
 		while (!s.testBit(0)) {
-			s = s.divide(new BigInteger("2"));
+			s = s.divide(BigInteger.valueOf(2));
 			r = r.add(BigInteger.ONE);
 		}
 		for (int i = 0; i < 20; i++) {
 			Random rand = new Random();
 			BigInteger a;
 			do {
-				a = new BigInteger(512, rand);
-			} while (a.compareTo(new BigInteger("2")) < 0
-					|| a.compareTo(a.pow(bitlengh).subtract(new BigInteger("2"))) > 0);
+				a = new BigInteger(bitlength, rand);
+			} while (a.compareTo(BigInteger.valueOf(2)) < 0 || a.compareTo(n.subtract(BigInteger.valueOf(2)))>0);
 			BigInteger x = a.modPow(s, n);
 			if (x.compareTo(BigInteger.ONE) == 0 || x.compareTo(n.subtract(BigInteger.ONE)) == 0) {
 				return true;
 			}
 			BigInteger j = BigInteger.ONE;
 			for (; j.compareTo(r.subtract(BigInteger.ONE)) < 0; j = j.add(BigInteger.ONE)) {
-				x = x.modPow(new BigInteger("2"), n);
+				x = x.modPow(BigInteger.valueOf(2), n);
 				if (x.equals(BigInteger.ONE)) {
 					return false;
 				}
@@ -54,11 +43,39 @@ public class Main {
 				}
 
 			}
-			
+
 		}
 
 		return false;
 	}
 
+	private static ArrayList<BigInteger> generator(int bitlength) {
+		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+		BigInteger b = BigInteger.valueOf(2).pow(bitlength - 1);
+		BigInteger temp;
+		BigInteger temp1;
+		long start = System.currentTimeMillis();
+		int nbr=1;
+		while (list.size() < 100) {
+			temp = new BigInteger(bitlength, new Random());
+			if (!temp.testBit(0)) {
+				temp = temp.add(BigInteger.ONE);
+			}
+			temp1 = b.add(temp);
+			if (rabinMillerTest(temp1, bitlength) && !list.contains(temp1)) {
+				list.add(temp1);
+				long time =System.currentTimeMillis() - start;
+				System.out.println("nbr: "+ nbr +" time: "+ time);
+				nbr++;
+				
+				
+				
+			}
+
+		}
+
+		return list;
+
+	}
 
 }
