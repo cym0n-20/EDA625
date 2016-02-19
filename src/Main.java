1080b50b3b1d15b2d3a2cd32aab1740da0ac93a7
cym0n-20 <simon.gustafsson20@hotmail.com>
2016-02-19 08:26:23 +0100

    Better design!

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("----------------------------------ASSIGNMENT 2-----------------------------------\n");
+		generator(512);
+		generator(1024);
+		generator(2048);
+		System.out.println("Assignment 2 complete");
+		System.out.println("----------------------------------ASSIGNMENT 3-----------------------------------\n");
+		RSAExponent();
 		
-		ArrayList<BigInteger> list = generator(512);
-		System.out.println("complete");
-
+	
 	}
 
 	public static boolean rabinMillerTest(BigInteger n, int bitlength) {
@@ -27,7 +33,7 @@ public class Main {
 			BigInteger a;
 			do {
 				a = new BigInteger(bitlength, rand);
-			} while (a.compareTo(BigInteger.valueOf(2)) < 0 || a.compareTo(n.subtract(BigInteger.valueOf(2)))>0);
+			} while (a.compareTo(BigInteger.valueOf(2)) < 0 || a.compareTo(n.subtract(BigInteger.valueOf(2))) > 0);
 			BigInteger x = a.modPow(s, n);
 			if (x.compareTo(BigInteger.ONE) == 0 || x.compareTo(n.subtract(BigInteger.ONE)) == 0) {
 				return true;
@@ -49,33 +55,83 @@ public class Main {
 		return false;
 	}
 
-	private static ArrayList<BigInteger> generator(int bitlength) {
+	private static void generator(int bitlength) {
 		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
 		BigInteger b = BigInteger.valueOf(2).pow(bitlength - 1);
 		BigInteger temp;
 		BigInteger temp1;
 		long start = System.currentTimeMillis();
-		int nbr=1;
+		long tot = 0;
+		int nbr = 1;
 		while (list.size() < 100) {
 			temp = new BigInteger(bitlength, new Random());
-			if (!temp.testBit(0)) {
-				temp = temp.add(BigInteger.ONE);
-			}
 			temp1 = b.add(temp);
 			if (rabinMillerTest(temp1, bitlength) && !list.contains(temp1)) {
 				list.add(temp1);
-				long time =System.currentTimeMillis() - start;
-				System.out.println("nbr: "+ nbr +" time: "+ time);
+				long time = System.currentTimeMillis() - start;
+				System.out.println("nbr: " + nbr + " time: " + time + "    value: " + temp1);
 				nbr++;
-				
-				
-				
+				tot = time;
 			}
 
 		}
-
-		return list;
+		System.out.println("Test with bitlength: "+ bitlength + " complited!\n Total time: "+ tot + " ms\n");
 
 	}
-
+	
+	private static BigInteger eucludeanAlgo(BigInteger m, BigInteger a){
+		BigInteger d1 = m;
+		BigInteger d2 = a;
+		BigInteger v1 = BigInteger.ZERO;
+		BigInteger v2 = BigInteger.ONE;
+		
+		while(!d2.equals(BigInteger.ZERO)){
+			BigInteger q = d1.divide(d2);
+			BigInteger t2 = v1.subtract(q.multiply(v2));
+			BigInteger t3 = d1.subtract(q.multiply(d2));
+			v1 = v2;
+			v2 = t2;
+			d1 = d2;
+			d2 = t3;
+			
+		}
+		BigInteger v=v1;
+		BigInteger d = d1;
+		
+		if(v.compareTo(BigInteger.ZERO)<0){
+			return v.add(m);
+		}
+		if(d.compareTo(BigInteger.ONE)>0){
+			return null;
+		}
+		
+		return v;
+		
+	}
+	
+	private static void RSAExponent(){
+		BigInteger p = new BigInteger(
+				"14439387693894199240705138671761644884574009876339628877673995849573528630478892694840078004210638934773188106051113021390538674243130589326729054983724673");
+		BigInteger q = new BigInteger(
+				"7341551497387753784433514062898919305791812005701318426076740460540616069112545818856877680345369712693892766934354460424979200377180578670689966389908181");
+		BigInteger m = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE).divide(BigInteger.valueOf(2)));
+		BigInteger e = BigInteger.valueOf((long) (Math.pow(2, 16)+1));
+		BigInteger d = eucludeanAlgo(m, e);
+		System.out.println("d value is: " + d);
+		BigInteger N = p.multiply(q);
+		BigInteger s;
+		do{
+			s =new BigInteger(512, new Random());
+		}while(!s.testBit(0));
+		System.out.println("s value is: " + s);
+		BigInteger c = s.modPow(e, N);
+		System.out.println("c value is: " + c);
+		BigInteger z = c.modPow(d,N);
+		System.out.println("z value is: " + z);
+		if(z.equals(s)){
+			System.out.println("It is correct");
+		}else{
+			System.out.println("Incorrect");
+		}
+	}
 }
